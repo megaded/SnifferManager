f526c87530a52275fcb5b554ea7c29c80d032e44
megaded <darkintel999@gmail.com>
2017-03-22 17:59:11 +0300

    fix

--- SnifferManager/Controllers/ReportController.cs
+++ SnifferManager/Controllers/ReportController.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SnifferManager.Reports;
+using SnifferManager.Models;
 
 namespace SnifferManager.Controllers
 {
+    [Error]
     public class ReportController : Controller
     {
-       [HttpPost]
-        public ActionResult LocationSales()
+        [HttpPost]
+        public ActionResult LocationSales(int locationId, DateTime beginDate, DateTime endDate)
         {
-            return View();
+            LocationSalesReport report = new LocationSalesReport();
+            var model = report.GetReport(locationId, beginDate, endDate);
+            return File(model, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "reportLocationSales.xlsx");
         }
-       
+
     }
 }
